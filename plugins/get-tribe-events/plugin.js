890162b71b1657e9cdd3240ee5dbc9d76ffb13d6
Patrick Johanneson <pat@patrickjohanneson.com>
2025-06-04 23:40:06 -0500

    Gather events, save as JSON file -- next up: get fullcalendar.io to read the JSON file

--- plugins/get-tribe-events/plugin.js
+++ plugins/get-tribe-events/plugin.js
@@ -6,6 +6,7 @@
  * @package get-tribe-events
  *
  * @link https://github.com/declanbyrd/eleventy-plugin-mastoarchive/
+ * @link https://piccalil.li/blog/create-json-feed-eleventy/ - Save to a JSON file
  */
 
 const Fetch = require( '@11ty/eleventy-fetch' );
@@ -17,14 +18,39 @@ module.exports = function( eleventyConfig, options ) {
 	}
 
 	const defaults = {
-		eventPath: '/wp-json/tribe/events/v1/events/this/is/clearly/wrong',
-		mode:      'production',
+		eventPath:       '/wp-json/tribe/events/v1/events/this/is/clearly/wrong',
+		mode:            'production',
+		exportPermalink: '/events.json',
 	}
 
 	const config = { ...defaults, ...options };
 
 	const EVENT_API         = `${config.baseURL}${config.eventPath}`;
 	const ITEMS_PER_REQUEST = 50;
+
+	/**
+	 * Generates the virtual NJK template.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return {string} The template NJK code.
+	 */
+	function eventsTemplate() {
+		return `
+{
+	"events": [
+	{% for item in collections.events %}
+		{
+			"id": "{{ item.id }}",
+			"title": "{{ item.title }}",
+			"url": "{{ item.url }}",
+			"startStr": "{{ item.start_date }}.000",
+			"endStr": "{{ item.end_date }}.000"
+		}{% if not loop.last %},{% endif %}
+	{% endfor %}
+	]
+}`;
+	}
 
 	/**
 	 * Gets a page of events from the REST API.
@@ -49,18 +75,23 @@ module.exports = function( eleventyConfig, options ) {
 			};
 
 			let json = await Fetch( url, {
-				duration: "1w",
+				duration: "6h",
 				type:     "json",
 			});
 
-			console.log( json );
+			// console.log( json );
 
-			return json;
+			return json.events;
 
 		} catch( e ) {
 			console.error( e );
 		}
 	}
+	eleventyConfig.addCollection( 'events', async (collectionsApi) => {
+		return getEvents();
+	} );
+	eleventyConfig.addTemplate( 'events-template.njk', eventsTemplate(), {
+		permalink: config.exportPermalink
+	} );
 
-	return getEvents();
 }
